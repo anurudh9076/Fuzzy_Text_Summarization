36d3aa62a88e09f460c7c5bd1aad45a9011b834a
anurudh9076 <anurudh9076@gmail.com>
2022-11-16 12:33:30 +0530

    create GUI

--- gui.py
+++ gui.py
@@ -1,40 +1,119 @@
+# Core Packages
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter.scrolledtext import *
+import tkinter.filedialog
+
 from tkinter import *
 from tkinter import filedialog
 from rouge import Rouge 
 import os
-import numpy as np
+
 #import rake_nltk
 
 
 
 
-root = Tk()
-my_filetypes = [('all files', '.*'), ('text files', '.txt')]
-f_value_arr=[]
-# root.filename1 =  filedialog.askopenfilename(title="D:\summary.txt",filetypes=my_filetypes)
-file_list=['001.txt']
-# for i in range(1):
-#     if(i!=20):
-#         file_list.append("0"+str(i)+".txt")
+ # Structure and Layout
+window = Tk()
+window.title("Summaryzer GUI")
+window.geometry("700x400")
+window.config(background='black')
+
+style = ttk.Style(window)
+style.configure('lefttab.TNotebook', tabposition='wn',)
 
 
-for file_last_name in file_list:
-    file_name=r'C:\Users\Hp\Desktop\Major_Fuzzy\data\bbc_news_data\article\business'+'\\'+file_last_name
-    print(file_name)
+# TAB LAYOUT
+tab_control = ttk.Notebook(window,style='lefttab.TNotebook')
  
-    # print ("File directory:",root.filename1)
-    print("\n")
-    root.withdraw()
-    #text=open(root.filename, encoding="utf-8").read()
-    text=open(file_name).read()
-    # print(text)
-    print("\n")
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+tab3 = ttk.Frame(tab_control)
+tab4 = ttk.Frame(tab_control)
+tab5 = ttk.Frame(tab_control)
+
+# ADD TABS TO NOTEBOOK
+tab_control.add(tab1, text=f'{"Home":^20s}')
+tab_control.add(tab2, text=f'{"File":^20s}')
+tab_control.add(tab3, text=f'{"URL":^20s}')
+tab_control.add(tab4, text=f'{"Comparer ":^20s}')
+tab_control.add(tab5, text=f'{"About ":^20s}')
 
 
+label1 = Label(tab1, text= 'Summaryzer',padx=5, pady=5)
+label1.grid(column=0, row=0)
+ 
+label2 = Label(tab2, text= 'File Processing',padx=5, pady=5)
+label2.grid(column=0, row=0)
 
+label3 = Label(tab3, text= 'URL',padx=5, pady=5)
+label3.grid(column=0, row=0)
+
+label3 = Label(tab4, text= 'Compare Summarizers',padx=5, pady=5)
+label3.grid(column=0, row=0)
+
+label4 = Label(tab5, text= 'About',padx=5, pady=5)
+label4.grid(column=0, row=0)
+
+tab_control.pack(expand=1, fill='both')
+
+# Functions 
+def get_summary():
+    pass
+	# raw_text = str(entry.get('1.0',tk.END))
+	# final_text = text_summarizer(raw_text)
+	# print(final_text)
+	# result = '\nSummary:{}'.format(final_text)
+	# tab1_display.insert(tk.END,result)
+
+
+# Clear entry widget
+def clear_text():
+	entry.delete('1.0',END)
+
+def clear_display_result():
+	tab1_display.delete('1.0',END)
+
+
+# Clear Text  with position 1.0
+def clear_text_file():
+	displayed_file.delete('1.0',END)
+
+# Clear Result of Functions
+def clear_text_result():
+	tab2_display_text.delete('1.0',END)
+
+# Clear For URL
+def clear_url_entry():
+	url_entry.delete(0,END)
+
+def clear_url_display():
+	tab3_display_text.delete('1.0',END)
+
+
+# Clear entry widget
+def clear_compare_text():
+	entry1.delete('1.0',END)
+
+def clear_compare_display_result():
+	tab1_display.delete('1.0',END)
+
+
+# Functions for TAB 2 FILE PROCESSER
+# Open File to Read and Process
+def openfiles():
+	file1 = tkinter.filedialog.askopenfilename(filetypes=(("Text Files",".txt"),("All files","*")))
+	read_text = open(file1).read()
+	displayed_file.insert(tk.END,read_text)
+
+
+def get_file_summary():
     # # Sentence segmentation
-    
+    text = displayed_file.get('1.0',tk.END)
     from nltk import sent_tokenize
+    import numpy as np
     sentences=(sent_tokenize(text))
     # print("Sentences:",sentences)
     # print("\n")
@@ -354,8 +433,8 @@ for file_last_name in file_list:
     senten['avg'] = fuzz.trimf(senten.universe, [0, 50, 100])
     senten['good'] = fuzz.trimf(senten.universe, [50, 100, 100])
     
-    senten.view()
-    input()
+    # senten.view()
+    
 
     rule1 = ctrl.Rule(position1['good'] & sentencelength['good'] & propernoun['good'] &numtokens['good'], senten['good'])
     rule2 = ctrl.Rule(position1['poor'] & sentencelength['poor'] & numtokens['poor'], senten['bad'])
@@ -408,7 +487,7 @@ for file_last_name in file_list:
     rule18,rule19,rule20,rule21,rule21,rule22,rule23,rule24,rule25]
     sent_ctrl = ctrl.ControlSystem(rule_list)
     Sent = ctrl.ControlSystemSimulation(sent_ctrl)
-    fuzzemptyarr= np.empty((20,1,2), dtype=object)
+    fuzzemptyarr= np.empty((2,1,2), dtype=object)
     t2=0
     summary2=[]
     for s in range(len(sentences)):
@@ -423,7 +502,7 @@ for file_last_name in file_list:
         Sent.input['numtokens'] = int(numeric_token[s])
     #Sent.input['service'] = 2
         Sent.compute()
-        if Sent.output['senten'] > 50:
+        if Sent.output['senten'] > 50 and t2<2:
             summary2.append((sentences[s]))
             fuzzemptyarr[t2][0][0] = sentences[s]
             fuzzemptyarr[t2][0][1] = s
@@ -490,48 +569,199 @@ for file_last_name in file_list:
 
     # print("success 1")
     # print(x)
-    print(file_last_name)
+    
     print("text is ================================================>\n")
     print(text)
 
     print("summary is ================================================>\n")
     print(res_summary)
     print(len(res_summary))
-    hyp_path=r'C:\Users\Hp\Desktop\Major_Fuzzy\data\bbc_news_data\summaries\Hypothesis\business'+r'\\'+file_last_name
-    ref_path=r'C:\Users\Hp\Desktop\Major_Fuzzy\data\bbc_news_data\summaries\reference\001.txt'
+    # hyp_path=r'C:\Users\Hp\Desktop\Major_Fuzzy\data\bbc_news_data\summaries\Hypothesis\business'+r'\\'+file_last_name
+    # ref_path=r'C:\Users\Hp\Desktop\Major_Fuzzy\data\bbc_news_data\summaries\reference\001.txt'
 
-    scores = files_rouge.get_scores(hyp_path, ref_path, avg=True)
-    print(scores)
+	
+	# final_text = text_summarizer(raw_text)
+	# result = '\nSummary:{}'.format(final_text)
+    tab2_display_text.insert(tk.END,res_summary)
 
-    import json
-   
-# declaringa a class
-    class obj:
-        
-        # constructor
-        def __init__(self, dict1):
-            self.__dict__.update(dict1)
-    
-    def dict2obj(dict1):
-        
-        # using json.loads method and passing json.dumps
-        # method and custom object hook as arguments
-        return json.loads(json.dumps(dict1), object_hook=obj)
-    obj1 = dict2obj(scores)
+# Fetch Text From Url
+# def get_text():
+# 	raw_text = str(url_entry.get())
+# 	page = urlopen(raw_text)
+# 	soup = BeautifulSoup(page)
+# 	fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
+# 	url_display.insert(tk.END,fetched_text)
 
-    # print(type(scores))
-    # print(obj1.rouge-1.f)
-    print(type(obj1))
-    # print(getattr(obj1))
-    rouge_1=getattr(obj1,'rouge-1')
-    f_value=getattr(rouge_1,'f')
-    f_value_arr.append(f_value)
-    print(f_value)
-    
-    print("\n")
-
-average = sum(f_value_arr)/len(f_value_arr)
-
-print("Average of list: ", round(average,5))
+# def get_url_summary():
+# 	raw_text = url_display.get('1.0',tk.END)
+# 	final_text = text_summarizer(raw_text)
+# 	result = '\nSummary:{}'.format(final_text)
+# 	tab3_display_text.insert(tk.END,result)	
 
 
+# COMPARER FUNCTIONS
+
+def use_spacy():
+	raw_text = str(entry1.get('1.0',tk.END))
+	final_text = text_summarizer(raw_text)
+	print(final_text)
+	result = '\nSpacy Summary:{}\n'.format(final_text)
+	tab4_display.insert(tk.END,result)
+
+def use_nltk():
+	raw_text = str(entry1.get('1.0',tk.END))
+	final_text = nltk_summarizer(raw_text)
+	print(final_text)
+	result = '\nNLTK Summary:{}\n'.format(final_text)
+	tab4_display.insert(tk.END,result)
+
+def use_gensim():
+	raw_text = str(entry1.get('1.0',tk.END))
+	final_text = summarize(raw_text)
+	print(final_text)
+	result = '\nGensim Summary:{}\n'.format(final_text)
+	tab4_display.insert(tk.END,result)
+
+def use_sumy():
+	raw_text = str(entry1.get('1.0',tk.END))
+	final_text = text_summarizer(raw_text)
+	print(final_text)
+	result = '\nSumy Summary:{}\n'.format(final_text)
+	tab4_display.insert(tk.END,result)
+
+# MAIN NLP TAB
+l1=Label(tab1,text="Enter Text To Summarize")
+l1.grid(row=1,column=0)
+
+entry=Text(tab1,height=10)
+entry.grid(row=2,column=0,columnspan=2,padx=5,pady=5)
+
+# BUTTONS
+button1=Button(tab1,text="Reset",command=clear_text, width=12,bg='#03A9F4',fg='#fff')
+button1.grid(row=4,column=0,padx=10,pady=10)
+
+button2=Button(tab1,text="Summarize",command=get_summary, width=12,bg='#ced',fg='#fff')
+button2.grid(row=4,column=1,padx=10,pady=10)
+
+button3=Button(tab1,text="Clear Result", command=clear_display_result,width=12,bg='#03A9F4',fg='#fff')
+button3.grid(row=5,column=0,padx=10,pady=10)
+
+button4=Button(tab1,text="Main Points", width=12,bg='#03A9F4',fg='#fff')
+button4.grid(row=5,column=1,padx=10,pady=10)
+
+# Display Screen For Result
+tab1_display = Text(tab1)
+tab1_display.grid(row=7,column=0, columnspan=3,padx=5,pady=5)
+
+
+#FILE PROCESSING TAB
+l1=Label(tab2,text="Open File To Summarize")
+l1.grid(row=1,column=1)
+
+displayed_file = ScrolledText(tab2,height=7)# Initial was Text(tab2)
+displayed_file.grid(row=2,column=0, columnspan=3,padx=5,pady=3)
+
+# BUTTONS FOR SECOND TAB/FILE READING TAB
+b0=Button(tab2,text="Open File", width=12,command=openfiles,bg='#c5cae9')
+b0.grid(row=3,column=0,padx=10,pady=10)
+
+b1=Button(tab2,text="Reset ", width=12,command=clear_text_file,bg="#b9f6ca")
+b1.grid(row=3,column=1,padx=10,pady=10)
+
+b2=Button(tab2,text="Summarize", width=12,command=get_file_summary,bg='blue',fg='#fff')
+b2.grid(row=3,column=2,padx=10,pady=10)
+
+b3=Button(tab2,text="Clear Result", width=12,command=clear_text_result)
+b3.grid(row=5,column=1,padx=10,pady=10)
+
+b4=Button(tab2,text="Close", width=12,command=window.destroy)
+b4.grid(row=5,column=2,padx=10,pady=10)
+
+# Display Screen
+# tab2_display_text = Text(tab2)
+tab2_display_text = ScrolledText(tab2,height=10)
+tab2_display_text.grid(row=7,column=0, columnspan=3,padx=5,pady=5)
+
+# Allows you to edit
+tab2_display_text.config(state=NORMAL)
+
+
+# URL TAB
+l1=Label(tab3,text="Enter URL To Summarize")
+l1.grid(row=1,column=0)
+
+raw_entry=StringVar()
+url_entry=Entry(tab3,textvariable=raw_entry,width=50)
+url_entry.grid(row=1,column=1)
+
+# BUTTONS
+# button1=Button(tab3,text="Reset",command=clear_url_entry, width=12,bg='#03A9F4',fg='#fff')
+# button1.grid(row=4,column=0,padx=10,pady=10)
+
+# button2=Button(tab3,text="Get Text",command=get_text, width=12,bg='#03A9F4',fg='#fff')
+# button2.grid(row=4,column=1,padx=10,pady=10)
+
+# button3=Button(tab3,text="Clear Result", command=clear_url_display,width=12,bg='#03A9F4',fg='#fff')
+# button3.grid(row=5,column=0,padx=10,pady=10)
+
+# button4=Button(tab3,text="Summarize",command=get_url_summary, width=12,bg='#03A9F4',fg='#fff')
+# button4.grid(row=5,column=1,padx=10,pady=10)
+
+# Display Screen For Result
+url_display = ScrolledText(tab3,height=10)
+url_display.grid(row=7,column=0, columnspan=3,padx=5,pady=5)
+
+
+tab3_display_text = ScrolledText(tab3,height=10)
+tab3_display_text.grid(row=10,column=0, columnspan=3,padx=5,pady=5)
+
+
+# COMPARER TAB
+l1=Label(tab4,text="Enter Text To Summarize")
+l1.grid(row=1,column=0)
+
+entry1=ScrolledText(tab4,height=10)
+entry1.grid(row=2,column=0,columnspan=3,padx=5,pady=3)
+
+# BUTTONS
+button1=Button(tab4,text="Reset",command=clear_compare_text, width=12,bg='#03A9F4',fg='#fff')
+button1.grid(row=4,column=0,padx=10,pady=10)
+
+button2=Button(tab4,text="SpaCy",command=use_spacy, width=12,bg='red',fg='#fff')
+button2.grid(row=4,column=1,padx=10,pady=10)
+
+button3=Button(tab4,text="Clear Result", command=clear_compare_display_result,width=12,bg='#03A9F4',fg='#fff')
+button3.grid(row=5,column=0,padx=10,pady=10)
+
+button4=Button(tab4,text="NLTK",command=use_nltk, width=12,bg='#03A9F4',fg='#fff')
+button4.grid(row=4,column=2,padx=10,pady=10)
+
+button4=Button(tab4,text="Gensim",command=use_gensim, width=12,bg='#03A9F4',fg='#fff')
+button4.grid(row=5,column=1,padx=10,pady=10)
+
+button4=Button(tab4,text="Sumy",command=use_sumy, width=12,bg='#03A9F4',fg='#fff')
+button4.grid(row=5,column=2,padx=10,pady=10)
+
+
+variable = StringVar()
+variable.set("SpaCy")
+choice_button = OptionMenu(tab4,variable,"SpaCy","Gensim","Sumy","NLTK")
+choice_button.grid(row=6,column=1)
+
+
+# Display Screen For Result
+tab4_display = ScrolledText(tab4,height=15)
+tab4_display.grid(row=7,column=0, columnspan=3,padx=5,pady=5)
+
+
+# About TAB
+about_label = Label(tab5,text="Summaryzer GUI V.0.0.1 \n Jesus saves @JCharisTech",pady=5,padx=5)
+about_label.grid(column=0,row=1)
+
+window.mainloop()
+
+
+
+# Jesse JCharis
+# J-Secur1ty
+# Jesus saves@JCharisTech
